eaccfc582ccd3646b62963997a3f6fd1105cbb06
Muhammad Ammar Abdurrahim <muhammadammar314@gmail.com>
2023-07-17 01:29:24 +0700

    update database 3

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,8 +3,8 @@ dotenv.config();
 
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('db_nutech', 'iv18e5ke5aqpq5sj16tw', 'pscale_pw_hnjvoImX02OPntuQz51ITmCNmCkp6d5Utf0R7dt41Jl',{
-    host : 'aws.connect.psdb.cloud',
+const db = new Sequelize(process.env.database, process.env.username, process.env.password,{
+    host : process.env.host,
     dialect : 'mysql',
     dialectOptions:{
         ssl: {
