b8f43f2c2fd3baeb974805b54cdcd8101d3e8822
Muhammad Ammar Abdurrahim <muhammadammar314@gmail.com>
2023-07-16 18:58:07 +0700

    update database

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,13 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('db_nutech', 'root', '',{
-    host : 'localhost',
-    dialect : 'mysql'
+const db = new Sequelize('db_nutech', 'r5m1vuz4jfrbb6jdb0xu', 'pscale_pw_L7KwinP9UqliPmQsjuoxalG3PiBeDWRGQNaSaSFQFcZ',{
+    host : 'aws.connect.psdb.cloud',
+    dialect : 'mysql',
+    dialectOptions:{
+        ssl: {
+            rejectUnauthorized: true,
+        }
+    }
 });
 
 export default db;
